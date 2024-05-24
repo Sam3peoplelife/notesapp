f2ad10c41e3d8fda0a1663ee7cf00274d21e03d7
Sam3peoplelife <kamishartem1@gmail.com>
2024-05-24 02:40:52 +0200

    register + overr some funcs

--- notesapp/base/urls.py
+++ notesapp/base/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import CustomLogoutView, CustomLoginView, TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView
+from .views import RegisterPage, CustomLogoutView, CustomLoginView, TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView
 
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view() , name='login'),
     path('logout/', CustomLogoutView.as_view(next_page='login'), name='logout'),
-    
+    path('register/', RegisterPage.as_view(), name='register'),
+
     path('', TaskList.as_view() , name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view() , name='task'),
     path('task-create/', TaskCreate.as_view() , name='task-create'),
